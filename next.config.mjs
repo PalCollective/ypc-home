7afffaaaac232c7596100031c338cfd701ac2594
snarkshow <laura.hw.chuang@gmail.com>
2024-03-19 13:11:42 -0400

    config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-	output: "export", // <=== enables static exports
-	reactStrictMode: true,
-	basePath: "/ypc-home",
+	output: "export",
 }
-
 export default nextConfig
