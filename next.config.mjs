b620f88c51c38e3dab5b8ea5c338242ed29fbcf0
snarkshow <laura.hw.chuang@gmail.com>
2024-03-19 14:33:43 -0400

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: "export",
+	sassOptions: {
+		includePaths: [path.join(__dirname, "src", "styles")],
+		prependData: `@import "_modulePrepend.scss"`,
+	},
 }
 export default nextConfig
