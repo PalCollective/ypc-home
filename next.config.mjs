be3131594e1f2a67ab9f743adddd5157501a0efe
snarkshow <laura.hw.chuang@gmail.com>
2024-03-19 13:00:50 -0400

    next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+	output: "export", // <=== enables static exports
+	reactStrictMode: true,
+	basePath: "/ypc-home",
+}
+
+export default nextConfig
