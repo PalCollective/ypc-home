28f1c2ef3d6f835740786c9cd53627dfff5a4772
snarkshow <laura.hw.chuang@gmail.com>
2024-03-19 13:43:53 -0400

    rollback

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,8 @@ const config: Config = {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-				hero: "url('.../public/aaron-blanco-tejedor-yH18lOSaZVQ-unsplash.jpg')",
+				logo: "url('/Logo.svg')",
+				hero: "url('/aaron-blanco-tejedor-yH18lOSaZVQ-unsplash.jpg')",
 			},
 		},
 	},
